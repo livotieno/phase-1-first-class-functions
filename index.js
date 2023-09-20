f926d83c0c4fb42cef8e33e7c8870efdbb33a37c
livotieno <livio5261@gmail.com>
2023-09-20 21:30:30 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,23 @@
+function  receivesAFunction (callback){
+    callback ();
+}
+//const receivesAFunction = (callback) => {callback();};
+
+
+ function returnsANamedFunction(namedFunction){
+    return function namedFunction(){
+ 
+ };
+}
+//const returnsANamedFunction = (namedFunction) => {return () => {};};
+
+ function returnsAnAnonymousFunction(anonymousFunction){
+    return function (){
+
+    };
+ }
+
+ 
+
+
+    
